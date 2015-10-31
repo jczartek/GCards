5276b33c0a3205ea633a817af73da6315cf0307d
Jakub Czartek <kuba@linux.pl>
2015-10-31 16:36:52 +0000

    Autotools: Add autogen.sh

---
+++ autogen.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+srcdir=$(dirname $0)
+test -z "$srcdir" && srcdir=.
+
+(test -f $srcdir/src/gc-main) || {
+  echo -n "**Error**: Directory "\`$srcdir\`" does not look the "
+  echo "top-level GCards directory."
+  exit 1
+}
+
+which gnome-autogen.sh || {
+  echo "You need to install gnome-common!"
+  exit 1
+}
+
+. gnome-autogen.sh
